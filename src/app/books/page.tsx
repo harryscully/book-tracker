39f20f8cdf99e44b42c6e266c1fe20ee1a740f61
harryscully <harry.scully@ugogroup.co.uk>
2026-03-07 17:18:08 +0000

    Render books on page

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,3 +1,19 @@
+"use client"
+import { useBooks } from "@/hooks/useBooks"
+import { BookModel } from "../../../generated/prisma/models/Book"
+
 export default function Books() {
-    return <h1>Books</h1>
+    const { isPending, isError, data, error } = useBooks()
+
+    if (isPending) return <p>Loading...</p>
+
+    if (isError) return <p>Error: {error.message}</p>
+
+    return (
+        <ul>
+            {data.map((book:BookModel) => (
+                <li key={book.id}>{book.title}</li>
+            ))}
+        </ul>
+    )
 }
