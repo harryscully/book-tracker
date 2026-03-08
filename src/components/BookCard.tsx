00c6be8c27a9de37198771e5d32279db0fe53a9b
harryscully <harry.scully@ugogroup.co.uk>
2026-03-08 20:17:13 +0000

    Badge colour for each status

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,12 +1,19 @@
 import type { BookModel } from "../../generated/prisma/models/Book"
 
-export default function BookCard({book}:{book: BookModel}) {
+export default function BookCard({ book }: { book: BookModel }) {
+
+    const statusColours = {
+        READING: "badge-info",
+        FINISHED: "badge-success",
+        WANT_TO_READ: "badge-warning"
+    }
+
     return (
-        <div key={book.id} className="card card-border bg-base-100 w-96">
+        <div className="card card-border bg-base-100 w-96">
             <div className="card-body">
                 <h2 className="card-title">
                     {book.title}
-                    <div className="badge badge-secondary">{book.status}</div>
+                    <div className={`badge ${statusColours[book.status]}`}>{book.status}</div>
                 </h2>
                 <p>{book.author}</p>
                 <div className="card-actions justify-end">
