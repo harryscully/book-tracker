0b00c2d25b1ca2132add1d4f54fa51ccf7a4d797
harryscully <harry.scully@ugogroup.co.uk>
2026-03-08 21:06:32 +0000

    Add POST to /api/books

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,7 +1,24 @@
-import { prisma } from "../../../../lib/prisma"
+import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
+import { bookSchema } from "@/lib/validations/book"
 
 export async function GET() {
     const books = await prisma.book.findMany()
     return NextResponse.json(books)
+}
+
+export async function POST(request: Request) {
+    const body = await request.json()
+    const result = bookSchema.safeParse(body)
+    if (!result.success) {
+        return NextResponse.json({ error: result.error }, { status: 400 })
+    } else {
+        const book = await prisma.book.create({
+            data: {
+                ...result.data,
+                userId: 1
+            }
+        })
+        return NextResponse.json(book, { status: 201 })
+    }
 }
