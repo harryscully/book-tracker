d43332f3e0f70e56001a85e75db7a3249c748aa3
harryscully <harry.scully@ugogroup.co.uk>
2026-03-08 20:13:24 +0000

    Extract card into BookCard component

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useBooks } from "@/hooks/useBooks"
 import type { BookModel } from "../../../generated/prisma/models/Book"
+import BookCard from "@/components/BookCard"
 
 export default function Books() {
     const { isPending, isError, data, error } = useBooks()
@@ -10,18 +11,7 @@ export default function Books() {
     if (isError) return <p>Error: {error.message}</p>
 
     const bookCards = data.map((book: BookModel) => (
-        <div key={book.id} className="card card-border bg-base-100 w-96">
-            <div className="card-body">
-                <h2 className="card-title">
-                    {book.title}
-                    <div className="badge badge-secondary">{book.status}</div>
-                </h2>
-                <p>{book.author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">{book.genre}</div>
-                </div>
-            </div>
-        </div>
+        <BookCard key={book.id} book={book} />
     ))
 
     return (
---
+++ src/components/BookCard.tsx
@@ -0,0 +1,18 @@
+import type { BookModel } from "../../generated/prisma/models/Book"
+
+export default function BookCard({book}:{book: BookModel}) {
+    return (
+        <div key={book.id} className="card card-border bg-base-100 w-96">
+            <div className="card-body">
+                <h2 className="card-title">
+                    {book.title}
+                    <div className="badge badge-secondary">{book.status}</div>
+                </h2>
+                <p>{book.author}</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">{book.genre}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
