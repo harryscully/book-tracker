50dbeb679b494adadcfa9ced68bcdc7a5a1017fa
harryscully <harry.scully@ugogroup.co.uk>
2026-03-09 11:39:46 +0000

    Add PATCH function to api route

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma"
+import { bookSchema } from "@/lib/validations/book"
 import { NextResponse } from "next/server"
 
 export async function DELETE(request: Request, { params }: { params: Promise<{id: string}>  }) {
@@ -9,4 +10,22 @@ export async function DELETE(request: Request, { params }: { params: Promise<{id
         }
     })
     return NextResponse.json({message: "deleted"}, { status: 202 })
+}
+
+export async function PATCH(request: Request, { params }: { params: Promise<{id: string}>  }) {
+    const { id } = await params
+    const body = await request.json()
+    const result = bookSchema.safeParse(body)
+    if (!result.success) {
+        return NextResponse.json({error: result.error}, {status: 400})
+    } else {
+        const book = await prisma.book.update({
+            where: {id: Number(id)},
+            data: { 
+                ...result.data
+            }}
+        )
+        return NextResponse.json(book, { status: 200 })
+    }
+    
 }
