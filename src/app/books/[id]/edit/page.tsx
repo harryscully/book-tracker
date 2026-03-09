9cbc67ad6eb80a1b4073f6124ee9a41c7d949f6a
harryscully <harry.scully@ugogroup.co.uk>
2026-03-09 12:57:54 +0000

    Add padding around the forms

--- src/app/books/[id]/edit/page.tsx
+++ src/app/books/[id]/edit/page.tsx
@@ -5,7 +5,7 @@ import EditBookForm from "@/components/EditBookForm";
 export default function EditBook() {
     const { id } = useParams()
     return (
-        <div className="flex flex-1 justify-center items-center">
+        <div className="flex flex-1 justify-center items-center px-4 py-6">
             <EditBookForm id={id as string} />
         </div>
     )
--- src/app/books/new/page.tsx
+++ src/app/books/new/page.tsx
@@ -2,7 +2,7 @@ import AddBookForm from "@/components/AddBookForm";
 
 export default function NewBook() {
     return (
-        <div className="flex flex-1 justify-center items-center">
+        <div className="flex flex-1 justify-center items-center px-4 py-6">
             <AddBookForm />
         </div>
     )
