7b1ba22e4225862de22ad6e7564964d4f5c44b7c
harryscully <harry.scully@ugogroup.co.uk>
2026-03-09 10:10:33 +0000

    Add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <h1>Book Tracker</h1>
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Welcome to <span className="italic highlight">book-tracker</span></h1>
+          <p className="py-6">
+            A simple app to track books you want to read, are currently reading, or books you've finished!
+          </p>
+          <Link href="/books" className="btn btn-primary">Go to Books</Link>
+        </div>
+      </div>
+    </div>
+  )
 }
